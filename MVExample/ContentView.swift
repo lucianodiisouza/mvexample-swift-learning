8fc1b0a5f183c00564647547e71063822dc5332c
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-06-30 17:56:03 -0300

    Initial Commit

---
+++ MVExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVExample
+//
+//  Created by Luciano dii Souza on 30/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVExample/MVExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVExampleApp.swift
+//  MVExample
+//
+//  Created by Luciano dii Souza on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MVExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
