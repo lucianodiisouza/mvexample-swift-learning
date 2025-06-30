dc496f21d6f8aaa6f36f0686e81f5e3a4e89fe18
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-06-30 20:40:38 -0300

    feat: wip StoreModel (MV)

---
+++ MVExample/Models/StoreModel.swift
@@ -0,0 +1,10 @@
+//
+//  StoreModel.swift
+//  MVExample
+//
+//  Created by Luciano dii Souza on 30/06/25.
+//
+
+class StoreModel: ObservableObject {
+    @Published var products: [Product] = []
+}
