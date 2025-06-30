e7ed87c65825c30c1a0f1bae2311182ffd83a1da
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-06-30 20:40:00 -0300

    feat: model Product

---
+++ MVExample/Models/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  MVExample
+//
+//  Created by Luciano dii Souza on 30/06/25.
+//
+
+import Foundation
+
+struct Product: Decodable {
+    var id: Int
+    var title: String
+    var price: Double
+}
