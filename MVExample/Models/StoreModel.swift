bd7627a403abbf28b99293d1c5e2fbf02dec7146
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-06-30 20:50:01 -0300

    feat: initialize webservice and place it on main thread

--- MVExample/Models/StoreModel.swift
+++ MVExample/Models/StoreModel.swift
@@ -5,6 +5,19 @@
 //  Created by Luciano dii Souza on 30/06/25.
 //
 
+import Foundation
+
+@MainActor
 class StoreModel: ObservableObject {
+    let webservice: Webservice
+    
     @Published var products: [Product] = []
+    
+    init(webservice: Webservice) {
+        self.webservice = webservice
+    }
+    
+    func populateProducts()  async throws {
+        products = try await webservice.getProducts()
+    }
 }
